86f3cd1b0ad1e21255d1b58458d949692f6d6236
TimmyLR <timmylr@users.noreply.github.com>
2026-08-04 20:19:34 +0800

    Fix: MongoDB driver v6 compatibility - remove deprecated topology.isConnected()

--- netlify/functions/utils/db.js
+++ netlify/functions/utils/db.js
@@ -1,19 +1,21 @@
 const { MongoClient } = require('mongodb');
 
 let cachedClient = null;
+let cachedDb = null;
 
 async function getDb() {
   const uri = process.env.MONGODB_URI;
   if (!uri) throw new Error('MONGODB_URI environment variable is not set');
 
-  if (cachedClient && cachedClient.topology && cachedClient.topology.isConnected()) {
-    return cachedClient.db('ntust_calendar');
+  if (cachedDb) {
+    return cachedDb;
   }
 
   const client = new MongoClient(uri);
   await client.connect();
   cachedClient = client;
-  return client.db('ntust_calendar');
+  cachedDb = client.db('ntust_calendar');
+  return cachedDb;
 }
 
 module.exports = { getDb };
